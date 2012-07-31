f5cebb732468b0286d4a7c467966631968515bc2
durium <renaud.gaubert@gmail.com>
2012-07-31 13:35:26 +0200

    Change of file

--- code/Main.cpp
+++ code/Main.cpp
@@ -1,5 +1,6 @@
 #include "Main.h"
 #include "LevelManager.h"
+#include <SkyX.h>
 
 Main::Main()
 {
@@ -39,4 +40,28 @@ void Main::createScene(void)
     // Create a Light and set its position
     Ogre::Light* light = mSceneMgr->createLight("MainLight");
     light->setPosition(20.0f, 80.0f, 50.0f);
+
+    //SkyX
+    SkyX::BasicController* mBasicController = new SkyX::BasicController();
+    SkyX::SkyX* mSkyX = new SkyX::SkyX(mSceneMgr, mBasicController);
+    mSkyX->create();
+
+    mBasicController->setMoonPhase(0.75f);
+
+    // Register listeners
+    mRoot->addFrameListener(mSkyX);
+    mWindow->addListener(mSkyX);
+
+    // Add the atmospheric scattering pass to our terrain material
+    // The ground atmospheric scattering pass must be added to all materials that are going to be used to
+    // render object which we want to have atmospheric scattering effects on.
+    mSkyX->getGPUManager()->addGroundPass(
+        static_cast<Ogre::MaterialPtr>(Ogre::MaterialManager::getSingleton().
+        getByName("Terrain"))->getTechnique(0)->createPass(), 5000, Ogre::SBT_TRANSPARENT_COLOUR);
+
+    // Create the terrain
+    mSceneMgr->setWorldGeometry("Terrain.cfg");
+
+    // Add a basic cloud layer
+    mSkyX->getCloudsManager()->add(SkyX::CloudLayer::Options(/* Default options */));
 }
