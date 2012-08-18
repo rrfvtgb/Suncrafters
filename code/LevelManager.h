3522f067fb05862e81614864e69e57220b073596
rrfvtgb <geof.brule@laposte.net>
2012-08-18 22:01:20 +0200

    Some starting of generation
    Creating function ...

--- code/LevelManager.h
+++ code/LevelManager.h
@@ -4,8 +4,13 @@
 #include "BaseApplication.h"
 #include "Chunk.h"
 #include "Block.h"
-#include "tinyXml/tinyxml.h"
+#include "tinyXml/XmlMgr.h"
+
 #include <fstream>
+#include <string>
+#include <math.h>
+// 10 000 / CHWIDTH
+#define MAPWIDTH  10000 / CHWIDTH
 
 class LevelManager
 {
@@ -14,7 +19,7 @@ class LevelManager
         virtual ~LevelManager();
 
         void createWorld();
-        void displayChunk(int x, int y);
+        void displayChunk(Ogre::Vector2 chunkCoord);
     protected:
     private:
         Ogre::SceneManager* mSceneMgr;
@@ -22,16 +27,24 @@ class LevelManager
         Block* mNonExistingBlock;
 
         std::ofstream flux;
-        TiXmlHandle* mHdl;
+        XmlMgr* mXmlManager;
 
-        Block* getProperXBlock(int x1, int y1, int x, int y, int z);
-        Block* getProperYBlock(int x1, int y1, int x, int y, int z);
-        Block* getProperZBlock(int x1, int y1, int x, int y, int z);
+        void manageVisibleFacesAt(Ogre::Vector2 chunkCoord, Ogre::Vector3 cubeCoord);
+        int realToInt(Ogre::Real num);
+        std::string getId(Ogre::Vector3 coord);
 
-        void manageVisibleFacesAt(int x1, int y1, int x, int y, int z);
-        void createFaceAt(std::string face, int x1, int y1, int x, int y, int z);
+        Block* getProperXBlock(Ogre::Vector2 chunkCoord, Ogre::Vector3 cubeCoord);
+        Block* getProperYBlock(Ogre::Vector2 chunkCoord, Ogre::Vector3 cubeCoord);
+        Block* getProperZBlock(Ogre::Vector2 chunkCoord, Ogre::Vector3 cubeCoord);
+
+        void createFaceAt(std::string face, Ogre::Vector2 chunkCoord, Ogre::Vector3 cubeCoord);
+
 
         std::string getCubeMaterialName(int texture, std::string face);
+
+        int getHeight(int x, int y, int seed);
+        Ogre::Vector2 getNumberFromCoord(int x, int y, int seed, int pass, int max);
+        int getNumber(int seed, int max, int min);
 };
 
 #endif // LEVELMANAGER_H
